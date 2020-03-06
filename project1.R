fae3c965b20797def5b838e70df78ebdcc544f6d
Jos3f <Jos3f@users.noreply.github.com>
2020-03-06 17:03:20 +0100

    Update project1.R

--- project1.R
+++ project1.R
@@ -21,20 +21,22 @@ summary(fit.lm)
 # Normal probability plot of the residuals
 par(mfrow = c(2, 2))
 qqnorm(rstandard(fit.lm))
+qqnorm(rstudent(fit.lm))
 qqnorm(fit.lm$residuals)
 # QQ-plot seems ok. just a little hint of a heavy tailed distribution. 
 
 # Plot of residuals versus the predicted response
 plot(fit.lm$fitted.values, fit.lm$residuals)
-plot(fit.lm$fitted.values, rstandard(fit.lm))
+plot(fit.lm$fitted.values, rstandard(fit.lm), xlab="Fitted values", ylab="standardized residuals")
+plot(fit.lm$fitted.values, rstudent(fit.lm), xlab="Fitted values", ylab="studentized residuals")
 # Uneven distribution of the values but otherwise it looks good. 
 
 
 # Print residuals versus every attribute (Not really useful?)
 par(mfrow = c(2, 2))
-for (column_index in colnames(data_to_use)){
-    print((data_to_use[[column_index]])[1]) 
-    plot(data_to_use[[column_index]], fit.lm$residuals, xlab=column_index,ylab="residual")
+for (column_index in colnames(data_to_use)[-1]){
+  print((data_to_use[[column_index]])[1]) 
+  plot(data_to_use[[column_index]], rstudent(fit.lm), xlab=column_index,ylab="studentized res")
 }
 # Seems like residuals depend on density. The Box - Cox Method might be useful
 
@@ -49,7 +51,8 @@ leverage.cutoff
 cooks.cutoff
 studres.cutoff
 
-par(mfrow = c(2, 3))
+#par(mfrow = c(2, 3))
+par(mfrow = c(1, 1))
 plot(fit.lm, which = 1:5, pch = 20)
 infl.lm <- influence.measures(fit.lm)
 summary(infl.lm)
@@ -92,52 +95,102 @@ eval <- eigen(WtW)
 eval
 condition_number <- sqrt(max(eval$values) / min(eval$values))
 condition_number # ( see p. 298 Montgomery)
+condition_number_no_sqrt <- (max(eval$values) / min(eval$values))
+condition_number_no_sqrt # ( see p. 298 Montgomery)
+
 
 # Variable selection:
 
 
-#Vi vill predikta salary genom att använda hitters
+#Vi vill predikta salary genom att anvï¿½nda hitters
 #Salary = 
 
 predict.regsubsets = function (object ,newdata ,id ,...){
- form=as.formula(object$call [[2]])
- mat=model.matrix(form ,newdata )
- coefi =coef(object ,id=id)
- xvars =names(coefi )
- mat[,xvars ]%*% coefi
- }
+  form=as.formula(object$call [[2]])
+  mat=model.matrix(form ,newdata )
+  coefi =coef(object ,id=id)
+  xvars =names(coefi )
+  mat[,xvars ]%*% coefi
+}
 
 
 methods =  list("backward","forward", "seqrep","exhaustive")
 
 for(m in methods){
+  
+  k=10
+  variables = 13
+  
+  set.seed (1)
+  folds=sample (1:k,nrow(data_to_use ),replace =TRUE)
+  cv.errors =matrix (NA ,k,variables, dimnames =list(NULL , paste (1:variables) ))
+  
+  r.sqr = matrix (NA ,k,variables, dimnames =list(NULL , paste (1:variables) ))
+  r.sqr.adj = matrix (NA ,k,variables, dimnames =list(NULL , paste (1:variables) ))
+  Cp = matrix (NA ,k,variables, dimnames =list(NULL , paste (1:variables) ))
+  
+  
+  for(j in 1:k){
+    best.fit = regsubsets (density~.,data=data_to_use[folds !=j,],method = m,
+                           nvmax =variables)
+    
+    for(i in 1:variables) {
+      pred=predict (best.fit ,data_to_use[folds ==j,], id=i)
+      cv.errors [j,i]=mean( (data_to_use$density[folds ==j]-pred)^2)
+      r.sqr [j,i]= (1 - ( sum((data_to_use$density[folds ==j]-pred)^2) / sum((data_to_use$density[folds ==j]-mean(data_to_use$density[folds ==j] ))^2 )) )
+      n = nrow(data_to_use[folds !=j,])
+      r.sqr.adj [j,i]= 1 - (1-r.sqr[j,i])*(n - 1)/(n-i-1)
+      n_test = nrow(data_to_use[folds ==j,])
+      Cp[j,i] = (sum((data_to_use$density[folds ==j]-pred)^2)/(summary(fit.lm)$sigma)^2) - n_test + 2*(i-1)
+      
+      
+      
+    }
+  }
+  
+  mean.cv.errors =apply(cv.errors ,2, mean)
+  par(mfrow =c(2,3))
+  plot(mean.cv.errors ,type='l', main=m)
+  points(which.min(mean.cv.errors),
+         mean.cv.errors[which.min(mean.cv.errors)],
+         col = "red", cex = 2, pch = 20)
+  
+  mean.r.sqr = apply(r.sqr ,2, mean)
+  plot(mean.r.sqr ,type='l', main=m, ylab = "R-squared", xlab = "Number of variables")
+  points(which.max(mean.r.sqr),
+         mean.r.sqr[which.max(mean.r.sqr)],
+         col = "red", cex = 2, pch = 20)
+  
+  mean.r.sqr.adj = apply(r.sqr.adj ,2, mean)
+  plot(mean.r.sqr.adj ,type='l', main=m, ylab = "adj-R-squared", xlab = "Number of variables")
+  points(which.max(mean.r.sqr.adj),
+         mean.r.sqr.adj[which.max(mean.r.sqr.adj)],
+         col = "red", cex = 2, pch = 20)
+  
+  mean.Cp = apply(Cp ,2, mean)
+  #plot(mean.Cp ,type='p', main=m, ylab = "Mallows's Cp", xlab = "Number of variables", ylim = c(0,20))
+  # lines(x = c(0,100), y = c(0,100),  lty=2 , col = "blue")
 
-k=10
-variables = 13
 
-set.seed (1)
-folds=sample (1:k,nrow(data_to_use ),replace =TRUE)
-cv.errors =matrix (NA ,k,variables, dimnames =list(NULL , paste (1:variables) ))
-
-
- for(j in 1:k){
-   best.fit = regsubsets (density~.,data=data_to_use[folds !=j,],method = m,
-                          nvmax =variables)
-   for(i in 1:variables) {
-     pred=predict (best.fit ,data_to_use[folds ==j,], id=i)
-     cv.errors [j,i]=mean( (data_to_use$density[folds ==j]-pred)^2)
-     }
-   }
-
-mean.cv.errors =apply(cv.errors ,2, mean)
-par(mfrow =c(1,1))
-plot(mean.cv.errors ,type='b', main=m)
-
-reg.best=regsubsets (density~.,data=data_to_use , nvmax =variables, method = m)
-summary(reg.best)
-plot(reg.best)
-coef(reg.best ,8)
-
+  
+  reg.best=regsubsets (density~.,data=data_to_use , nvmax =variables, method = m)
+  reg.best.summary <- summary(reg.best)
+  plot(reg.best.summary$bic,
+       xlab = "Number of Variables ", ylab = "BIC", type = "l")
+  points(which.min(reg.best.summary$bic),
+         reg.best.summary$bic[which.min(reg.best.summary$bic)],
+         col = "red", cex = 2, pch = 20)
+  
+  plot(reg.best.summary$cp,
+       xlab = "Number of Variables ", ylab = "Mallows's Cp", type = "l")
+  points(which.min(reg.best.summary$cp),
+         reg.best.summary$cp[which.min(reg.best.summary$cp)],
+         col = "red", cex = 2, pch = 20)
+  
+  
+  # plot(reg.best)
+  print(reg.best.summary)
+  coef(reg.best ,8)
 }
 
 
@@ -187,4 +240,3 @@ fit.lm.boot <- Boot(fit.lm, method="residual", R=1000) # Smaller R gives larger 
 summary(fit.lm.boot)
 confint(fit.lm.boot)
 hist(fit.lm.boot)
-
